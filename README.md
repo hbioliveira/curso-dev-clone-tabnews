42f69b1de27a7719c21ba7ba8db9bccd031cc6f4
Gabriela Oliveira <hbiloliveira@gmail.com>
2024-03-18 00:27:44 +0000

    adicionar scripts e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # curso-dev-clone-tabnews
-Implementação do https://www.tabnews.com.br para o  https://www.curso.dev
+
+Implementação do https://www.tabnews.com.br para o https://www.curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
 export default function Home() {
-    return <h1>Alguma frase pra alguém s2git push </h1>
+  return <h1>Alguma frase pra alguém s2git push </h1>;
+}
+
+function teste() {
+  console.log("indentação errada");
 }
